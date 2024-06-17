e4ae15627788e7243c209f8233c252af1af0dd5e
tomo520473 <hasep.ss@gmail.com>
2024-06-17 23:56:22 +0900

    make sg

---
+++ security_group.tf
@@ -0,0 +1,126 @@
+# ------------------------
+# Security Group
+# ------------------------
+# web seulity group
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web front role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-web-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "web_in_http" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_in_https" {
+  security_group_id = aws_security_group.web_sg.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "web_out_tcp3000" {
+  security_group_id        = aws_security_group.web_sg.id
+  type                     = "egress"
+  from_port                = 3000
+  to_port                  = 3000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+# app seulity group
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project}-${var.environment}-app-sg"
+  description = "application server role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# opmng seulity group
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.environment}-opmng-sg"
+  description = "operation and management role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-opmg-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_out_http" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_out_https" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# db seulity group
+resource "aws_security_group" "db_sg" {
+  name        = "${var.project}-${var.environment}-db-sg"
+  description = "database role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-db-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "db_in_tcp3306" {
+  security_group_id        = aws_security_group.db_sg.id
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.app_sg.id
+}
