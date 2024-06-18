d2e6c0555be2d640e75b35070b055c0c5a6009d1
tomo520473 <hasep.ss@gmail.com>
2024-06-18 22:07:28 +0900

    make rds subnet group

--- rds.tf
+++ rds.tf
@@ -25,3 +25,20 @@ resource "aws_db_option_group" "mysql_standalone_opriongroup" {
   engine_name          = "mysql"
   major_engine_version = "8.0"
 }
+
+# ------------------------
+# subnet group
+# ------------------------
+resource "aws_db_subnet_group" "mysql_standalone_subnetgroup" {
+  name = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+    Project = var.project
+    Env     = var.environment
+  }
+}
