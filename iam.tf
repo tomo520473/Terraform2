355b007f7dc195d68ce59d0bdb536de87854b288
tomo520473 <hasep.ss@gmail.com>
2024-06-19 22:35:53 +0900

    attach policy

--- iam.tf
+++ iam.tf
@@ -6,7 +6,6 @@ resource "aws_iam_role" "app_iam_role" {
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
-
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -18,3 +17,22 @@ data "aws_iam_policy_document" "ec2_assume_role" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_iam_role_s3_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
