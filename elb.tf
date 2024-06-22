e195d564eaa2e140b209e9892d04558126dcd0dc
tomo520473 <hasep.ss@gmail.com>
2024-06-22 17:03:02 +0900

    setting for https

--- elb.tf
+++ elb.tf
@@ -9,6 +9,7 @@ resource "aws_lb" "alb" {
   subnets            = [aws_subnet.public_subnet_1a.id, aws_subnet.public_subnet_1c.id]
 }
 
+// http
 resource "aws_lb_listener" "alb_listener_http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
@@ -21,6 +22,20 @@ resource "aws_lb_listener" "alb_listener_http" {
   }
 }
 
+// https
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = aws_acm_certificate.tokyo_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
 
 # ------------------------
 # target group
