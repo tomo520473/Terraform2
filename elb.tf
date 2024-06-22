c26015214e3c414a34337b208d738d7ba90fa44f
tomo520473 <hasep.ss@gmail.com>
2024-06-22 13:48:01 +0900

    make target group

--- elb.tf
+++ elb.tf
@@ -8,3 +8,23 @@ resource "aws_alb" "alb" {
   security_groups    = [aws_security_group.web_sg.id]
   subnets            = [aws_subnet.public_subnet_1a.id, aws_subnet.public_subnet_1c.id]
 }
+
+# ------------------------
+# target group
+# ------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    name    = "${var.project}-${var.environment}-app-tg"
+    project = var.project
+    Env     = var.environment
+  }
+}
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
